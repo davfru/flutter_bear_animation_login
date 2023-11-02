c74b26b8e80697f34578198cc7e0f99905cc0ff5
davfru <davide.fruci@assistdigital.com>
2023-11-02 23:13:52 +0100

    rive impl

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bear_animation_login/core/theme/app_color.dart';
+import 'package:rive/rive.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +9,197 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(),
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+          backgroundColor: Color(0xFFD6E2EA),
+          body: SingleChildScrollView(
+            child: HomeScreen(),
+          )),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen> {
+  // email
+  FocusNode emailFocusNode = FocusNode();
+  TextEditingController emailController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // password
+  FocusNode passwordFocusNode = FocusNode();
+  TextEditingController passwordController = TextEditingController();
+
+  // rive animation input
+  StateMachineController? controller;
+
+  SMIInput<bool>? isChecking;
+  SMIInput<double>? numLook;
+  SMIInput<bool>? isHandsUp;
+
+  SMIInput<bool>? trigSuccess;
+  SMIInput<bool>? trigFail;
+
+  @override
+  void initState() {
+    emailFocusNode.addListener(emailFocus);
+    passwordFocusNode.addListener(passwordFocus);
+  }
+
+  @override
+  void dispose() {
+    emailFocusNode.removeListener(emailFocus);
+    passwordFocusNode.removeListener(passwordFocus);
+  }
+
+  void emailFocus() {
+    isChecking?.change(emailFocusNode.hasFocus);
+  }
+
+  void passwordFocus() {
+    isHandsUp?.change(passwordFocusNode.hasFocus);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        children: [
+          SizedBox(
+              height: 250,
+              width: 250,
+              child: RiveAnimation.asset(
+                "assets/login-teddy.riv",
+                fit: BoxFit.fitHeight,
+                stateMachines: const ["Login Machine"],
+                onInit: (artboard) {
+                  controller = StateMachineController.fromArtboard(artboard,
+                      "Login Machine"); // Login Machine from rive editor when open this asset rive
+                  if (controller == null) return;
+
+                  artboard.addController(controller!);
+                  isChecking = controller?.findInput("isChecking");
+                  numLook = controller?.findInput("numLook");
+                  isHandsUp = controller?.findInput("isHandsUp");
+                  trigSuccess = controller?.findInput("trigSuccess");
+                  trigFail = controller?.findInput("trigFail");
+                },
+              )),
+          Container(
+            decoration: BoxDecoration(
+                color: AppColor.white, borderRadius: BorderRadius.circular(16)),
+            padding: const EdgeInsets.all(16),
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: TextField(
+                  focusNode: emailFocusNode,
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                      border: InputBorder.none, hintText: "Email"),
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  onChanged: (value) {
+                    numLook?.change(value.length.toDouble());
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: TextField(
+                  focusNode: passwordFocusNode,
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                      border: InputBorder.none, hintText: "Password"),
+                  obscureText: true,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  onChanged: (value) {},
+                ),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 64,
+                child: ElevatedButton(
+                    onPressed: () async {
+                      emailFocusNode.unfocus();
+                      passwordFocusNode.unfocus();
+
+                      final email = emailController.text;
+                      final password = passwordController.text;
+
+                      showLoadingDialog(context);
+
+                      await Future.delayed(const Duration(milliseconds: 2000));
+
+                      if (mounted) Navigator.pop(context);
+
+                      if (email == "mail@mail.com" && password == "1234") {
+                        trigSuccess?.change(true);
+                      } else {
+                        trigFail?.change(true);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16))),
+                    child: const Text("Login")),
+              )
+            ]),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+void showLoadingDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (_) {
+      return WillPopScope(
+        onWillPop: () async => false,
+        child: Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 32),
+                child: CircularProgressIndicator(),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
 }
